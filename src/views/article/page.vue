b5698c1daf948bc7e544427f498e5fd56d3f4056
lingongbing <462441355@qq.com>
2022-10-16 10:08:37 +0800

    * add

--- src/views/article/page.vue
+++ src/views/article/page.vue
@@ -54,7 +54,7 @@
           <span v-if="row.is_del == 2">隐藏</span>
         </template>
       </el-table-column>
-      <el-table-column label="操作" prop="id" align="center">
+      <el-table-column label="操作" prop="id" align="center" width="300px">
         <template slot-scope="{row}">
           <el-button v-if="row.is_topping == 1" type="primary">置顶</el-button>
           <el-button v-if="row.is_topping == 2" type="primary">取消置顶</el-button>
@@ -66,7 +66,7 @@
       </el-table-column>
     </el-table>
 
-    <el-pagination background layout="prev, pager, next" :total="total" :page.sync="listQuery.page" :limit.sync="listQuery.pageSize" @pagination="getList" />
+    <el-pagination :page-size="listQuery.pageSize" :pager-count="11" layout="prev, pager, next" :total="total" @current-change="handleCurrentChange" />
   </div>
 </template>
 
@@ -121,14 +121,18 @@ export default {
       this.activeData = res.data.activeData
       this.total = res.data.count
     },
+    // 搜索
     handleFilter() {
-
+      this.init()
     },
+    // 创建文章
     handleCreate() {
 
     },
-    getList() {
-
+    // 分页
+    handleCurrentChange(val) {
+      this.listQuery.page = val
+      this.init()
     }
   }
 }
